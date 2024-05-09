f1cff1feca26c334bb1ee9a02443ded59a1baab6
Karim Abboud <98329660+Kabboud@users.noreply.github.com>
2024-05-09 00:56:16 +0000

    Add --no-pager option to systemctl status to prevent hang while awaiting user exit

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -7,7 +7,7 @@ db_password=$3
 
 # Start docker
 # Make sure you understand the double pipe operator
-sudo systemctl status docker || systemctl start docker
+sudo systemctl status --no-pager docker || systemctl start docker
 
 # Check container status (try the following cmds on terminal)
 docker container inspect jrvs-psql
