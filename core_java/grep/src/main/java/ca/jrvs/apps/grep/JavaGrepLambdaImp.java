1c2c78a517543863839cdc9592de2b3993185546
Karim Abboud <98329660+Kabboud@users.noreply.github.com>
2024-06-11 11:31:02 -0400

    simplify grep stream operations

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
@@ -5,20 +5,24 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JavaGrepLambdaImp extends JavaGrepImp implements JavaGrepLambda {
+public class JavaGrepLambdaImp extends JavaGrepImp {
 
   final Logger logger = LoggerFactory.getLogger(JavaGrepLambdaImp.class);
 
   @Override
-  public Stream<File> listFilesStream(String rootDir) {
-    try (Stream<Path> fileStream = Files.walk(Paths.get(rootDir))) {
+  public List<File> listFiles(String rootDir) {
+    Path rootPath = Paths.get(rootDir);
+
+    try (Stream<Path> fileStream = Files.walk(rootPath)) {
       return fileStream.filter(file -> !Files.isDirectory(file))
-          .map(Path::toFile);
+          .map(Path::toFile).collect(Collectors.toList());
     } catch (IOException e) {
       logger.error("Encountered error while listing files", e);
       throw new RuntimeException(e);
@@ -26,19 +30,20 @@ public class JavaGrepLambdaImp extends JavaGrepImp implements JavaGrepLambda {
   }
 
   @Override
-  public Stream<String> readLinesStream(File inputFile) {
+  public List<String> readLines(File inputFile) {
+    if (inputFile.toString().endsWith(".class")) {
+      return super.readLines(inputFile);
+    }
     if (!inputFile.isFile()) {
       throw new IllegalArgumentException("Provided inputFile argument is not a file: " + inputFile);
     }
 
-    Stream<String> stream;
-    try {
-      stream = Files.lines(inputFile.toPath());
+    try (Stream<String> lines = Files.lines(inputFile.toPath())) {
+      return lines.collect(Collectors.toList());
     } catch (IOException e) {
       logger.error("Encountered error while reading file.", e);
       throw new RuntimeException(e);
     }
-    return stream;
   }
 
   public static void main(String[] args) {
